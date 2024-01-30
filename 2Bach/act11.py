8fcba37ffa00d83a8a94581e6d79dbf06ec91e8e
BeaterGG <allende.alemany@gmail.com>
2024-01-30 13:04:57 +0100

    30/1/2024

---
+++ 2Bach/act11.py
@@ -0,0 +1,28 @@
+class triangulo:
+    def __init__(self,base,altura) -> None:
+        tipo="Triángulo"
+        lados=3
+        self.tipo = tipo
+        self.lados = lados
+        self.base = base
+        self.altura = altura
+    def props(self):
+            print("Numero de lados:", self.lados)
+            print("Tipo de polígono:", self.tipo)
+            print("Base:", self.base)
+            print("Altura:", self.altura)
+    
+
+t1 = triangulo(5,4)
+
+print("---- Datos segundo triángulo ---")
+
+base=input("Introduce la longitud de la base: ")
+altura=input("Introduce la longitud de la altura: ")
+
+t2= triangulo(base,altura)
+
+print("-------- Propiedades del primer triángulo--------")
+t1.props()
+print("-------- Propiedades del segundo triángulo--------")
+t2.props()
